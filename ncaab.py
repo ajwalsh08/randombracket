9c6207aa9117c63de5b76bd53c21396af9d79086
alex walsh <alexander.walsh@gmail.com>
2014-03-20 13:16:33 -0500

    The program now lets the user define the number of upsets they think will happen.

--- ncaab.py
+++ ncaab.py
@@ -6,14 +6,32 @@ import random
 # Bring in the favorites and their chances of winning as a dictionary.
 
 with open('chances.txt') as f:
-	d = dict(line.strip().split(",") for line in f)
+	favorites = dict(line.strip().split(",") for line in f)
 
-# Use random to determine an "outcome" for each game.
+# Create a function that chooses winners using random numbers generation.
 
-for team in d:
-	outcome = random.randrange(1,100001)	
-	chance = float(d[team]) * 1000
-	if outcome < int(chance):
-		pass
-	else:
-		print str(team)
+def losers(d):
+	lst = list() 
+	for team in d:
+		outcome = random.randrange(1,100001)	
+		chance = float(d[team]) * 1000
+		if outcome < int(chance):
+			pass
+		else:
+			lst.append(str(team))
+	return lst
+
+# Ask the user how many upsets they think will happen. 
+
+upsets = raw_input("How many upsets do you think will happen?")
+
+# Run the function once.
+
+picks = losers(favorites)
+
+# Check its length. If it returns more than 5 teams, run it again. Else, print it.
+
+while len(picks) != int(upsets):
+	picks = losers(favorites)
+else: 
+	print picks
